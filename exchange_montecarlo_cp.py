90c40d5dc4dd3d702a8264b4e449394418db7237
shimagaki <kai0shimagaki@gmail.com>
2015-11-07 10:35:49 +0900

    I've been writing rewriteing_gibbus_sampling.py

---
+++ exchange_montecarlo_cp.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+#標準多変量正規分布
+def standard_multivariate_normal_pdf(x):
+    x = np.asarray(x)    
+    return 1.0/(2.0*np.pi)**(len(x)/2.0)*np.exp(-0.5*np.dot(x,x))
+#混合ガウシアンの平均値を指定して生成する関数
+def generate_equilibrium_distribution(mu):
+    #定常分布（混合ガウシアン）
+    def equilibrium_distribution(x):    
+        x = np.asarray(x)
+        #混合比率(5:5で混ぜる)
+        theta = np.array((0.5, 0.5))
+        #２つのガウシアンの確率密度（平均(mu,mu), (-mu,-mu)の２つ）
+        p1 = standard_multivariate_normal_pdf(x-np.array(( mu,  mu)))
+        p2 = standard_multivariate_normal_pdf(x-np.array((-mu, -mu)))
+        #混合分布の確率密度
+        return np.dot(theta, np.array((p1,p2)))
+    return equilibrium_distribution    
+#MCMC(メトロポリス法）による次候補生成
+def next_with_metropolis_algorithm(x,equilibrium_distribution):
+    #次の位置の候補の作成    
+    x_proposed = x + 0.5*np.random.normal(size=len(x))
+    #メトロポリスアルゴリズムに従って候補の棄却・採用判定を行う    
+    if(np.random.uniform(size=1) < equilibrium_distribution(x_proposed)/equilibrium_distribution(x)):
+        return x_proposed
+    else:
+        return x
+#通常のマルコフ連鎖モンテカルロ法による分布生成
+def mcmc_normal(mu, size_simulation):
+    #定常分布の平均値（(-mu,-mu）,(mu,mu))設定
+    equilibrium_distribution = generate_equilibrium_distribution(mu)    
+    #初期スタート位置
+    x = np.array([[0,0]])    
+    for i in range(size_simulation):
+        x = np.vstack((x, next_with_metropolis_algorithm(x[-1],equilibrium_distribution)))
+        #print("x =  \n", x)
+    return x
+#レプリカ交換モンテカルロ法による分布生成
+def mcmc_replica_exchange(mu, size_simulation, size_replica, frequency_exchange):
+    #初期スタート位置
+    x = [[np.random.uniform(-mu,mu,2)] for i in range(size_replica)]
+    #適当に間を切って定常分布"族"生成
+    #mu=3, REPLICA=5なので0.75grid
+    equilibrium_distributions = [generate_equilibrium_distribution(var) for var in np.linspace(0,mu,num=size_replica)]
+    print("ed = \n",equilibrium_distributions)
+    #シミュレーション    
+    for i in range(size_simulation):
+        #通常のMCMCシミュレーション（メトロポリス法）
+        for index_replica in range(size_replica):
+            x_next = next_with_metropolis_algorithm(x[index_replica][-1],equilibrium_distributions[index_replica])
+            x[index_replica] = np.vstack((x[index_replica], x_next))
+        #適当な交換頻度に応じたレプリカの交換
+        if(i % frequency_exchange == 0):
+            index_exchange = int(np.random.uniform(0,size_replica-1))
+            x1 = x[index_exchange  ][-1]                
+            x2 = x[index_exchange+1][-1]
+            eq_dist1 = equilibrium_distributions[index_exchange]            
+            eq_dist2 = equilibrium_distributions[index_exchange+1]            
+            #メトロポリスアルゴリズムに従って状態交換の採択・棄却判定
+            if(np.random.uniform(size=1) < (eq_dist1(x2)*eq_dist2(x1))/(eq_dist1(x1)*eq_dist2(x2))):
+                x[index_exchange][-1], x[index_exchange+1][-1] = np.copy(x2),np.copy(x1)
+    return x[size_replica-1]
+#サンプルの散布図　＆　真の確率密度の等高線
+def plot_result(x, mu):    
+    equilibrium_distribution = generate_equilibrium_distribution(MU)    
+    axis_range = mu+3.0
+    #散布図
+    plt.scatter(x[:,0],x[:,1],c="black",s=10,edgecolors="None",alpha=0.5)    
+    #等高線（真の確率密度から計算）
+    mesh = np.arange(-axis_range,axis_range,0.1)
+    X,Y = np.meshgrid(mesh, mesh)
+    Z = np.zeros(np.shape(X))
+    for i in range(np.size(X,0)):
+        for j in range(np.size(X,1)):
+            Z[i,j] = equilibrium_distribution((X[i,j],Y[i,j]))
+    CS = plt.contour(X,Y,Z,linewidths=5)
+    plt.clabel(CS,fontsize=15)
+    #各ガウシアンの平均値が見やすいように適用に線を入れる    
+    plt.axvline(x= mu,c='r',ls='--')
+    plt.axvline(x=-mu,c='b',ls='--')
+    plt.axhline(y= mu,c='r',ls='--')
+    plt.axhline(y=-mu,c='b',ls='--')
+    plt.axis([-axis_range, axis_range,-axis_range, axis_range])
+    plt.show()
+#
+if __name__ == "__main__":
+    #定常分布にしたい混合ガウシアンの平均値(２つのガウシアンにおいて(mu,mu), (-mu,-mu)が平均となる)
+    MU = 3.0
+    #レプリカの個数
+    SIZE_REPLICA = 5
+    #何ステップに一回状態交換を計算するか
+    FREQUENCY_EXCHANGE = 20
+    #シミュレーション回数
+    SIZE_SIMULATION= 10000
+
+    #通常のマルコフ連鎖モンテカルロ法による混合ガウス分布生成
+    x = mcmc_normal(MU,SIZE_SIMULATION)
+    plot_result(x, MU) 
+    #print( np.mean(x, axis=0) )
+
+    #レプリカ交換モンテカルロ法による混合ガウス分布生成
+    x = mcmc_replica_exchange(MU,SIZE_SIMULATION,SIZE_REPLICA,FREQUENCY_EXCHANGE)
+    plot_result(x, MU)
+    print( np.mean(x, axis=0))
---
+++ gibbus_sampling3.py
@@ -0,0 +1,76 @@
+#############################################################
+#	generate n dimensional smpling that is that{(x1,x2,....,xn)}
+#	in this case parameter theta is assume given
+#       modification: sample valiables from the part of sequence. 
+##############################################################
+import numpy as np
+from scipy import linalg
+np.random.seed(0)
+################### set of prob parameters ###################
+n = 8  # number of Ising variables
+T = 800 # number of epoc for theta
+N = 10000 # number of smples, comes form true Prob
+N_est = 100 # number of smples, comes form estimated Prob
+ypc = 1 # descent ratio
+cord = 0 # changiable spin variable when gibbus sampling time step
+beta = 0.01
+# set symmetric theta matrix
+theta = np.arange(1,n+1)
+theta = np.tensordot(theta[:, np.newaxis], theta[: , np.newaxis], axes = ([1],[1]))
+np.fill_diagonal(theta, 0)
+print("theta = \n", theta)
+theta_est =  n * np.random.rand(n,n)    # create same size of matrix
+print("theta_est = \n", theta_est)
+# set del theta mat
+del_l_del_theta = np.empty_like(theta) # sum of x_i* x_j for each sample
+del_l_del_theta_est = np.random.rand(n,n) # sum of x_i* x_j for each sample
+#X = np.ones(n)
+#X_est = np.ones(n)
+
+X =  2 * np.array(np.random.random_integers(0,1,n) - 0.5)
+X_est =  2 * np.array(np.random.random_integers(0,1,n) - 0.5)
+
+# this func obtain one set of (x1,...,xn), which is distribute Boltzman waight
+# t_wait_sample : waiting time to get sample, each time step is correspond to the update step on Gibbus sampling
+def gen_a_gibbus_sample(t_wait_sample,X = [], theta=[[]]):
+    # local minimamu が沢山ある分布なので、最初の状態でどれを選ぶかに大きく依存してしまう
+    # Gibbus samplingはもちろん部分列を取り出さなければならない
+    # sampling方法の修正、順番に分布関数を変化させていくが、samplingはランダムに部分列を取り出すことにする。
+    # -> 変化可能な変数の選択をランダムにしてlocal minima にはまらないようにしよう
+    global cord # this is selected 
+    for i in range(t_wait_sample):# 20 = 収束するまでの更新回数
+        cord = (cord + 1 ) % n 
+        #print("twait = ", t_wait_sample, " cord = ", cord)
+        #cord = np.random.randint(n) # select f
+        #valu = 0.1 * sum(  np.array( np.tensordot( X ,theta, axes = ([0],[1]) ) ) ) - X[cord] * theta[cord][cord] 
+        valu = beta *  ( np.tensordot(X, theta[:,cord], axes = ([0],[0]) ) -X[cord] * theta[cord][cord] ) 
+        r = np.exp( -valu ) / ( np.exp( -valu ) + np.exp( valu ) )
+        R = np.random.uniform(0,1)
+        if (R <= r):
+            X[cord] =  1
+        else:
+            X[cord] = -1
+    return X
+# t_wait_sample 回数のupdate後のsampleが一つ手元に入ってく
+# ここまででthetaが与えられたら、sampleを生成できるようになった。
+
+# make a matrix (del_l_theta)_ij , Sum_k (x^k_i, x^k_j) matrix,k is sample index,  k = 1,...,N
+# N_start, N_endの区間のみを使用するように
+def get_del_l_del_theta_mat(N, X = [] , theta = [[]], del_l_del_theta = [[]]):
+    for k in range(N):
+        rand = np.random.randint(5,n * 10) # mean of rand is 20, which is same with previous one
+        X = gen_a_gibbus_sample(rand, X , theta)
+        del_l_del_theta = del_l_del_theta + np.tensordot(X[:,np.newaxis],X[:,np.newaxis], axes = ([1],[1]))
+    return del_l_del_theta / N
+# ここまでで勾配をもとめる各thetaのij微分がもとまるようになった。
+############################ MAIN ###############################
+del_l_del_theta = get_del_l_del_theta_mat(N, X,theta, del_l_del_theta)
+# 以下では1sampleでdel_l_del_theta_matを求めて、thetaを更新してを繰り返す。
+for t in range(T):
+    # update theta using Graduant Descent
+    theta_est = theta_est -  ypc * ( del_l_del_theta - del_l_del_theta_est )
+    #theta_est = theta_est -  ypc * ( - del_l_del_theta_est )
+    # sampling using t-th theta
+    del_l_del_theta_est = get_del_l_del_theta_mat(N_est, X_est, theta_est, del_l_del_theta_est)
+    print( (np.absolute( theta - theta_est ).sum() ))
+print("theta_est = \n", theta_est)
