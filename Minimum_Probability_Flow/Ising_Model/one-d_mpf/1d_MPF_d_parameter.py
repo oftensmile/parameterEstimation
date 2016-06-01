853725c3365f5d079d83cf968020b94d9c5258b2
shimagaki <kai0shimagaki@gmail.com>
2016-06-01 21:31:51 +0900

    Update

--- Minimum_Probability_Flow/Ising_Model/one-d_mpf/1d_MPF_d_parameter.py
+++ Minimum_Probability_Flow/Ising_Model/one-d_mpf/1d_MPF_d_parameter.py
@@ -19,11 +19,11 @@ dT=T_max/n_T
 t_burn_emp, t_burn_model = 100, 10#10000, 100
 t_interval = 10
 #parameter ( System )
-d, N_sample = 32,1000 #124, 1000
+d, N_sample = 16,300 #124, 1000
 #parameter ( MPF+GD )
 lr,eps =0.1, 1.0e-100
 #n_mfa = 100 #Number of the sample for Mean Field Aproximation.
-t_gd_max=50 
+t_gd_max=110 
 def gen_mcmc(J1,J2,x=[] ):
     for i in range(d):
         #Heat Bath
@@ -52,43 +52,28 @@ for n in range(N_sample):
 
 #MPF
 #theta_model1,theta_model2=3.0, 2.0  #Initial Guess
-theta_model=np.random.uniform(3,4,d)    #Initial guess
+theta_model=np.random.uniform(0,4,d)    #Initial guess
+init_theta=np.copy(theta_model)
 print("#diff_E diff_E1_nin diff_E2_nin")
 for t_gd in range(t_gd_max):
     gradK=np.zeros(d)
-    #gradK1,gradK2=0.0,0.0
     n_bach=len(X_sample)
     for nin in range(n_bach):
         x_nin=np.copy(X_sample[nin])
-        #gradK1_nin,gradK2_nin=0.0,0.0
         gradK_nin=np.zeros(d)
         #calc gradK_nin_one_by_one
         for l in range(d):
-            #diff_delE1_nin=-2.0*x_nin[hd]*(x_nin[(hd+d-1)%d]+x_nin[(hd+1)%d])
-            #diff_delE2_nin=-2.0*x_nin[hd]*(x_nin[(hd+d-2)%d]+x_nin[(hd+2)%d])
             diff_del_l_nin=-1.0*x_nin[l]*x_nin[(l+1)%d]
             gradK_nin[l]=2.0*diff_del_l_nin*np.exp(diff_del_l_nin*theta_model[l])
-            #diff_E1_nin=diff_delE1_nin*theta_model1
-            #diff_E2_nin=diff_delE2_nin*theta_model2
-            #diff_E_nin=diff_E1_nin+diff_E2_nin
-            #gradK1_nin+=diff_delE1_nin*np.exp(0.5*diff_E_nin)/d
-            #gradK2_nin+=diff_delE2_nin*np.exp(0.5*diff_E_nin)/d
         gradK=np.copy(gradK)+gradK_nin*(1.0/n_bach)
     theta_model=theta_model-lr*gradK
-    #gradK1+=gradK1_nin/n_bach
-    #gradK2+=gradK2_nin/n_bach
-    #theta_model1=theta_model1 - lr * gradK1
-    #theta_model2=theta_model2 - lr * gradK2
-    #print("theta_model1=",theta_model1,"gradK1=",gradK1,"theta_model2=",gradK2)
-    #theta_diff1=abs(theta_model1-J1)
-    #theta_diff2=abs(theta_model2-J2)
     error_func=np.sum(np.abs(theta_model-J_vec))/d
-    #print(t_gd,np.abs(gradK1),np.abs(gradK2),theta_diff1,theta_diff2)
     print(t_gd,error_func)
-#print("#theta1,theta2 (true)=",J1,J2,"theta1,theta2 _estimated=",theta_model1,theta_model2)
 bins=np.arange(1,d+1)
 bar_width=0.2
 plt.bar(bins,J_vec,color="blue",width=bar_width,label="true",align="center")
 plt.bar(bins+bar_width,theta_model,color="red",width=bar_width,label="estimated",align="center")
+plt.bar(bins+2*bar_width,init_theta,color="green",width=bar_width,label="inital",align="center")
+plt.bar(bins+3*bar_width,gradK*100,color="gray",width=bar_width,label="gradK",align="center")
 plt.legend()
 plt.show()
