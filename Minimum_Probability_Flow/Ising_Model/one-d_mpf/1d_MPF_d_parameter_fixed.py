f286ac41ccfdc230b12517eac2a926a146739fe6
shimagaki <kai0shimagaki@gmail.com>
2016-06-08 08:45:41 +0900

    Added MPF_2parameter_2d.py

--- Minimum_Probability_Flow/Ising_Model/one-d_mpf/1d_MPF_d_parameter_fixed.py
+++ Minimum_Probability_Flow/Ising_Model/one-d_mpf/1d_MPF_d_parameter_fixed.py
@@ -7,12 +7,12 @@ import time
 from scipy import linalg
 import matplotlib.pyplot as plt
 import csv 
-np.random.seed(2)
+np.random.seed(0)
 #parameter ( MCMC )
 #t_burn_emp, t_burn_model = 1100, 10#10000, 100
 t_interval = 40
 #parameter ( System )
-d, N_sample = 32,1100 #124, 1000
+d, N_sample = 16,300 #124, 1000
 N_remove = 100
 #parameter ( MPF+GD )
 lr,eps =0.1, 1.0e-100
@@ -23,7 +23,6 @@ def gen_mcmc(J=[],x=[] ):
         diff_E=-2.0*x[i]*(J[i]*x[(d+1)%d]+J[(i+d-1)%d]*x[(i+d-1)%d])
         r=1.0/(1+np.exp(diff_E)) 
         #r=np.exp(-diff_E) 
-        
         R=np.random.uniform(0,1)
         if(R<=r):
             x[i]=x[i]*(-1)
@@ -55,11 +54,10 @@ for t_gd in range(t_gd_max):
             xl_xl_plu_1=x_nin[l]*x_nin[(l+1)%d]
             xl_min_1_xl=x_nin[(l+d-1)%d]*x_nin[l]
             xl_plu_1_xl_pul_2=x_nin[(l+1)%d]*x_nin[(l+2)%d]
-            gradK_nin[l]=  xl_xl_plu_1*np.exp(   xl_xl_plu_1*theta_model[l] ) *(1.0/d)
+            gradK_nin[l]= xl_xl_plu_1*np.exp( xl_xl_plu_1*theta_model[l] ) *(1.0/d)
             gradK_nin[l]*= ( np.exp(xl_min_1_xl*theta_model[(l+d-1)%d])+np.exp(xl_plu_1_xl_pul_2*theta_model[(l+1)%d]) )
-        gradK=np.copy(gradK)+gradK_nin*(1.0/n_bach)
+            gradK[l]+=gradK_nin[l]/n_bach
     theta_model=theta_model-lr*gradK
-    #theta_model=theta_model-lr*(gradK + 0.005*theta_model)
     error_func=np.sum(np.abs(theta_model-J_vec))/d
     print(t_gd,error_func)
 #Plot
@@ -70,6 +68,6 @@ plt.bar(bins+bar_width,theta_model,color="red",width=bar_width,label="estimated"
 plt.bar(bins+2*bar_width,init_theta,color="green",width=bar_width,label="initial",align="center")
 plt.bar(bins+3*bar_width,gradK*10,color="gray",width=bar_width,label="gradK",align="center")
 plt.legend()
-filename="test_output_fixed5.png"
+filename="test_output_fixed6.png"
 plt.savefig(filename)
 plt.show()
--- Minimum_Probability_Flow/Ising_Model/one-d_mpf/MPF_2parameter_2d.py
+++ Minimum_Probability_Flow/Ising_Model/one-d_mpf/MPF_2parameter_2d.py
@@ -8,87 +8,60 @@ from scipy import linalg
 import matplotlib.pyplot as plt
 import csv
 np.random.seed(0)
-#parameter ( Model )
-T_max=1.2
-#Temperature Dependance
-#= J^-1=kT/J=T/Tc, Tc=J/k=1
-n_T=100
-dT=T_max/n_T 
-
 #parameter ( MCMC )
-#t_burn_emp, t_burn_model = 1000, 10#10000, 100
-t_interval = 60
+t_interval = 40
 #parameter ( System )
-d, N_sample = 4,150 #124, 1000
-N_remove = 100
+d_x,d_y, N_sample = 4,4,400 #124, 1000
+N_remove=40
 #parameter ( MPF+GD )
-lr,eps =0.01, 1.0e-100
-#n_mfa = 100 #Number of the sample for Mean Field Aproximation.
-t_gd_max=40 
-def gen_mcmc(J1,J2,x=[[]] ):
-    for i in range(d):
-        for j in range(d):
+lr,eps =0.1, 1.0e-100
+t_gd_max=200 
+def gen_mcmc(J1,J2,x=[] ):
+    for ix in range(d_x):
+        for iy in range(d_y):
             #Heat Bath
-            diff_E=2.0*J1*x[i][j]*(x[(i+d-1)%d][j]+x[(i+1)%d][j]) + 2.0*J2*x[i][j]*(x[i][(j+d-1)%d]+x[i][(j+1)%d])# E_new-E_old
-            #r=1.0/(1+np.exp(diff_E)) 
-            r=1#np.exp(-diff_E) 
+            diff_E=-2.0*x[ix+iy*d_x]*( J1*(x[(ix+d_x-1)%d_x+iy*d_x]+x[(ix+1)%d_x]) + J2* (x[ix+d_x*((iy+d_y-1)%d_y)]+x[ix+d_x*((iy+1)%d_y)]) )#E_new-E_old
+            r=1.0/(1+np.exp(diff_E)) 
             R=np.random.uniform(0,1)
             if(R<=r):
-                x[i][j]=x[i][j]*(-1)
+                x[ix+iy*d_x]=x[ix+iy*d_x]*(-1)
     return x
 
 #######    MAIN    ########
 #Generate sample-dist
-J1,J2=0.0,0.0 # =theta_sample
-x = np.random.uniform(-1,1,(d,d))
+J1,J2=1.0,1.0 # =theta_sample
+x = np.random.uniform(-1,1,d_x*d_y)
 x = np.array(np.sign(x))
-#SAMPLIN
-
-n_J=100
-J_max,J_min=2.0, -2.0
-dJ=(J_max-J_min)/n_J
+#SAMPLING
 for n in range(N_sample):
     for t in range(t_interval):
         x = np.copy(gen_mcmc(J1,J2,x))
-    if(n==N_remove):
-        X_sample = np.reshape(np.copy(x),len(x)*len(x.T))
-    elif(n>N_remove):
-        X_sample=np.vstack((X_sample,np.reshape(np.copy(x),len(x)*len(x.T))))
-n_bach=len(X_sample)
-theta_model1,theta_model2=1.5,1.5   #Initial Guess
-print("X_sample=\n",X_sample)
-
-mean_magnet=0.0
-for n in range(n_bach):
-    x_nin=np.copy(X_sample[n])
-    print(x_nin)
-    sum_magnet=np.sum(x_nin)/(len(x_nin))
-    mean_magnet+=sum_magnet/n_bach
-print("magnetization=",mean_magnet)
-
-
-"""
-print("#gd-step, abs-grad_likelihood, theta-error")
+        if(n==N_remove):X_sample = np.copy(x)
+        elif(n>N_remove):X_sample=np.vstack((X_sample,np.copy(x)))
+#MPF
+theta_model1,theta_model2=3.0, 2.0  #Initial Guess
+print("#diff_E diff_E1_nin diff_E2_nin")
 for t_gd in range(t_gd_max):
-    #calc gradK of theta
     gradK1,gradK2=0.0,0.0
-    for sample in X_sample:
-        x_nin=np.reshape(np.copy(sample),(d,d))
-        gradK_nin1,gradK_nin2=0.0,0.0
-        #hamming distance = 1
-        for ni in range(d):
-            for nj in range(d):
-                diff_delE_nin1=-2.0*x_nin[ni][nj]*(x_nin[(ni+d-1)%d][nj]+x_nin[(ni+1)%d][nj])
-                diff_delE_nin2=-2.0*x_nin[ni][nj]*(x_nin[ni][(nj+d-1)%d]+x_nin[ni][(nj+1)%d])
-                diff_E_nin=diff_delE_nin1*theta_model1 + diff_delE_nin2*theta_model2
-                gradK_nin1+=diff_delE_nin1*np.exp(0.5*diff_E_nin)
-                gradK_nin2+=diff_delE_nin2*np.exp(0.5*diff_E_nin)
-        gradK1+=gradK_nin1*(1.0/n_bach)
-        gradK2+=gradK_nin2*(1.0/n_bach)
-    theta_model1=np.copy(theta_model1) - lr * gradK1
-    theta_model2=np.copy(theta_model2) - lr * gradK2
-    theta_diff1,theta_diff2=abs(theta_model1-J1),abs(theta_model2-J2)
-    print(t_gd,abs(gradK1),abs(gradK2),theta_diff1,theta_diff2)
-print("#J1true,J2true=",J1,J2,"the1,the2=",theta_model1,theta_model2)
-
-"""
+    n_bach=len(X_sample)
+    for nin in range(n_bach):
+        x_nin=np.copy(X_sample[nin])
+        gradK1_nin,gradK2_nin=0.0,0.0
+        for ix in range(d_x):
+            for iy in range(d_y):
+                #diff_E=E(x_new)-E(x_old)
+                diff_delE1_nin=x_nin[ix+iy*d_x]*(x[(ix+d_x-1)%d_x+iy*d_x]+x[(ix+1)%d_x])
+                diff_delE2_nin=x_nin[ix+iy*d_x]*(x[((iy+d_y-1)%d_y)]+x[ix+d_x*((iy+1)%d_y)])
+                diff_E1_nin=diff_delE1_nin*theta_model1
+                diff_E2_nin=diff_delE2_nin*theta_model2
+                diff_E_nin=diff_E1_nin+diff_E2_nin
+                gradK1_nin+=diff_delE1_nin*np.exp(diff_E_nin)/(d_x*d_y)
+                gradK2_nin+=diff_delE2_nin*np.exp(diff_E_nin)/(d_x*d_y)
+        gradK1+=gradK1_nin/n_bach
+        gradK2+=gradK2_nin/n_bach
+    theta_model1=theta_model1 - lr * gradK1
+    theta_model2=theta_model2 - lr * gradK2
+    theta_diff1=abs(theta_model1-J1)
+    theta_diff2=abs(theta_model2-J2)
+    print(t_gd,np.abs(gradK1),np.abs(gradK2),theta_diff1,theta_diff2)
+print("#theta1,theta2 (true)=",J1,J2,"theta1,theta2 _estimated=",theta_model1,theta_model2)
