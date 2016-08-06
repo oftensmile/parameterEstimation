e8f76be89d053a27f72dc6311d86e877cd0b2364
shimagaki <kai0shimagaki@gmail.com>
2016-08-06 15:51:26 +0900

    Added test-room/

--- Minimum_Probability_Flow/Ising_Model/one-d_mpf/test-room/1d_MPF_d_para_correct_bias.py
+++ Minimum_Probability_Flow/Ising_Model/one-d_mpf/test-room/1d_MPF_d_para_correct_bias.py
@@ -7,16 +7,18 @@ import time
 from scipy import linalg
 import matplotlib.pyplot as plt
 import csv 
-np.random.seed(0)
+np.random.seed(10)
 #parameter ( MCMC )
 #t_burn_emp, t_burn_model = 1100, 10#10000, 100
 t_interval = 40
 #parameter ( System )
-d, N_sample = 16,300 #124, 1000
+#d, N_sample = 16,300 #124, 1000
+d, N_sample =16,300 #124, 1000
+#N_remove = 100
 N_remove = 100
 #parameter ( MPF+GD )
 lr,eps =0.1, 1.0e-100
-t_gd_max=400 
+t_gd_max=1000 
 def gen_mcmc(J=[],x=[] ):
     for i in range(d):
         #Heat Bath
@@ -34,13 +36,13 @@ def calc_E(x_tot=[[]],theta=[[]]):
     for n in range(len_x):
         x_n=np.copy(x_tot[n])
         E[n]=np.matrix(x_n)*np.matrix(theta)*np.matrix(x_n).T
-        print("E(n)=",E[n])
     return E
 
 #######    MAIN    ########
 ##Generate sample-dist
 J_max,J_min=1.0,0.0
 J_vec=np.random.uniform(J_min,J_max,d)
+
 x = np.random.uniform(-1,1,d)
 x = np.array(np.sign(x))
 ##SAMPLING
@@ -49,7 +51,6 @@ for n in range(N_sample+N_remove):
         x = np.copy(gen_mcmc(J_vec,x))
     if(n==N_remove):X_sample = np.copy(x)
     elif(n>N_remove):X_sample=np.vstack((X_sample,np.copy(x)))
-
 ###MPF
 
 ##Find Hamming Distance 1 whith in the data.
@@ -60,7 +61,6 @@ idx=np.where(dist_mat!=1)
 dist_mat2=np.copy(dist_mat)
 dist_mat2[idx]=0    #Dist of none zero elemens are 1 hamming distance.
 ##
-
 len_idx=len(idx[0])
 for i in range(len_idx):
     diff_sample_pair_i=X_sample[idx[0][i]]-X_sample[idx[1][i]]
@@ -69,39 +69,34 @@ for i in range(len_idx):
 #In this case I applied 
 theta_model=np.random.uniform(0,4,d)    #Initial guess
 init_theta=np.copy(theta_model)
-print("#diff_E diff_E1_nin diff_E2_nin")
 for t_gd in range(t_gd_max):
     gradK=np.zeros(d)
     n_bach=len(X_sample)
     for nin in range(n_bach):
         x_nin=np.copy(X_sample[nin])
         gradK_nin=np.zeros(d)
-        for l in range(d):
-            xl_xl_plu_1=x_nin[l]*x_nin[(l+1)%d]
-            xl_min_1_xl=x_nin[(l+d-1)%d]*x_nin[l]
-            xl_plu_1_xl_pul_2=x_nin[(l+1)%d]*x_nin[(l+2)%d]
-            gradK_nin[l]= - xl_xl_plu_1*np.exp( -xl_xl_plu_1*theta_model[l] ) /d
-            gradK_nin[l]*= ( np.exp(-xl_min_1_xl*theta_model[(l+d-1)%d])+np.exp(-xl_plu_1_xl_pul_2*theta_model[(l+1)%d]) )
-            gradK[l]+=gradK_nin[l]/n_bach
+        idx2=np.where(dist_mat2[nin]==1)
         #To eliminate transiton of data to data.
         #idx2 includes index of sample.
-        idx2=np.where(dist_mat2[nin]==1)
-        for i in idx2:
-            diff_sample_pair_i=X_sample[idx[0][i]]-X_sample[idx[1][i]]
-            #print("diff_sample_pair_i=\n",diff_sample_pair_i)
-            idx3=np.where(diff_sample_pair_i!=0)
-            print("idx3=",idx3)
-            l=idx3[0][0]
-        #for l in idx3:
-            #xl_xl_plu_1=x_nin[l]*x_nin[(l+1)%d]
-            #xl_min_1_xl=x_nin[(l+d-1)%d]*x_nin[l]
-            #xl_plu_1_xl_pul_2=x_nin[(l+1)%d]*x_nin[(l+2)%d]
-            # OPPOSITE SIGN
-            #gradK_nin[l]= - xl_xl_plu_1*np.exp( -xl_xl_plu_1*theta_model[l] ) /d
-            #gradK_nin[l]*= ( np.exp(-xl_min_1_xl*theta_model[(l+d-1)%d])+np.exp(-xl_plu_1_xl_pul_2*theta_model[(l+1)%d]) )
-            gradK[l]-=gradK_nin[l]/n_bach
-    
-   
+        check_list=np.zeros(d)
+        if(len(idx2[0]>0)):
+            for i in idx2[0]:
+                diff_sample_pair_i=X_sample[nin]-X_sample[i]
+                idx3=np.where(diff_sample_pair_i!=0)
+                l2=idx3[0][0]
+                if(check_list[l2]==0):
+                    #gradK[l2]-=gradK_nin[l2]/n_bach
+                    check_list[l2]+=1
+
+        for l in range(d):
+            if(check_list[l]==0):
+                xl_xl_plu_1=x_nin[l]*x_nin[(l+1)%d]
+                xl_min_1_xl=x_nin[(l+d-1)%d]*x_nin[l]
+                xl_plu_1_xl_pul_2=x_nin[(l+1)%d]*x_nin[(l+2)%d]
+                gradK_nin[l]= - xl_xl_plu_1*np.exp( -xl_xl_plu_1*theta_model[l] ) /d
+                gradK_nin[l]*= ( np.exp(-xl_min_1_xl*theta_model[(l+d-1)%d])+np.exp(-xl_plu_1_xl_pul_2*theta_model[(l+1)%d]) )
+                gradK[l]+=gradK_nin[l]/n_bach
+                   
     theta_model=theta_model-lr*gradK
     sum_of_gradK=np.sum(gradK)
     error_func=np.sum(np.abs(theta_model-J_vec))/d
@@ -114,6 +109,7 @@ plt.bar(bins+bar_width,theta_model,color="red",width=bar_width,label="estimated"
 plt.bar(bins+2*bar_width,init_theta,color="green",width=bar_width,label="initial",align="center")
 plt.bar(bins+3*bar_width,gradK*10,color="gray",width=bar_width,label="gradK",align="center")
 plt.legend()
+plt.title("With bias correction")
 #filename="test_output_fixed6.png"
 #plt.savefig(filename)
 plt.show()
