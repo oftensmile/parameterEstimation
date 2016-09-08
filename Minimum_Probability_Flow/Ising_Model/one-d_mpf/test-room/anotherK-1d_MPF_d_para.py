c0ca30cfc1f0d67757586d2b1dd153d7a7c9ca01
shimagaki <kai0shimagaki@gmail.com>
2016-09-08 11:35:24 +0900

    Added anotherK-1d_MPF_d_para.py

---
+++ Minimum_Probability_Flow/Ising_Model/one-d_mpf/test-room/anotherK-1d_MPF_d_para.py
@@ -0,0 +1,100 @@
+#2016/08/05
+##############
+#   H = -J*sum(xixj), J in R^1
+##############
+import numpy as np
+import time 
+from scipy import linalg
+import matplotlib.pyplot as plt
+import csv 
+np.random.seed(10)
+#parameter ( MCMC )
+#t_burn_emp, t_burn_model = 1100, 10#10000, 100
+t_interval = 40
+#parameter ( System )
+d, N_sample =16,800 #124, 1000
+N_remove = 100
+#parameter ( MPF+GD )
+lr,eps =0.1, 1.0e-100
+t_gd_max=2000 
+def gen_mcmc(J=[],x=[] ):
+    for i in range(d):
+        #Heat Bath
+        diff_E=2.0*x[i]*(J[i]*x[(d+1+i)%d]+J[(i+d-1)%d]*x[(i+d-1)%d])
+        r=1.0/(1+np.exp(diff_E)) 
+        #r=np.exp(-diff_E) 
+        R=np.random.uniform(0,1)
+        if(R<=r):
+            x[i]=x[i]*(-1)
+    return x
+
+def calc_E(x_tot=[[]],theta=[[]]):
+    len_x=len(x_tot)
+    E=np.zeros(len_x)
+    for n in range(len_x):
+        x_n=np.copy(x_tot[n])
+        E[n]=np.matrix(x_n)*np.matrix(theta)*np.matrix(x_n).T
+    return E
+
+#######    MAIN    ########
+##Generate sample-dist
+J_max,J_min=1.0,0.0
+J_vec=np.random.uniform(J_min,J_max,d)
+
+x = np.random.uniform(-1,1,d)
+x = np.array(np.sign(x))
+##SAMPLING
+for n in range(N_sample+N_remove):
+    for t in range(t_interval):
+        x = np.copy(gen_mcmc(J_vec,x))
+    if(n==N_remove):X_sample = np.copy(x)
+    elif(n>N_remove):X_sample=np.vstack((X_sample,np.copy(x)))
+###MPF
+
+##Find Hamming Distance 1 whith in the data.
+dist_mat=d*np.ones((N_sample,N_sample))
+dist_mat=2*np.copy(dist_mat)-2*(np.matrix(X_sample)*np.matrix(X_sample).T)
+dist_mat/=4
+idx=np.where(dist_mat!=1)
+dist_mat2=np.copy(dist_mat)
+dist_mat2[idx]=0    #Dist of none zero elemens are 1 hamming distance.
+#In this case I applied 
+theta_model=np.random.uniform(0,4,d)    #Initial guess
+init_theta=np.copy(theta_model)
+n_bach=len(X_sample)
+for t_gd in range(t_gd_max):
+    gradK=np.zeros(d)
+    for nin in range(n_bach):
+        x_nin=np.copy(X_sample[nin])
+        gradK_nin=np.zeros(d)
+        idx2=np.where(dist_mat2.T[nin]==1)
+        #To eliminate transiton of data to data.
+        check_list=np.zeros(d)
+        #""" 
+        #Extracting only balanced flow.
+        normalize=d
+        if(len(idx2[0]>0)):
+            for i in idx2[0]:
+                diff_sample_pair_i=X_sample[nin]-X_sample[i]
+                idx3=np.where(diff_sample_pair_i!=0)
+                l2=idx3[0][0]
+                if(check_list[l2]==0):
+                    normalize-=1
+                    check_list[l2]+=1
+        #"""
+        for l in range(d):
+            if(check_list[l]==0):
+            #if(check_list[l]==1):
+                xl_xl_plu_1=x_nin[l]*x_nin[(l+1)%d]
+                xl_min_1_xl=x_nin[(l+d-1)%d]*x_nin[l]
+                xl_plu_1_xl_pul_2=x_nin[(l+1)%d]*x_nin[(l+2)%d]
+                El=2*(xl_xl_plu_1*theta_model[l]+xl_min_1_xl+theta_model[(l-1+d)%d])
+                El_plu_1=2*(xl_xl_plu_1*theta_model[l]+xl_plu_1_xl_pul_2+theta_model[(l+1)%d])
+                gradK_nin[l]=-2*xl_xl_plu_1*( np.exp(El)/(1+np.exp(El))**2 + np.exp(El_plu_1)/(1+np.exp(El_plu_1))**2 )
+                gradK[l]+=gradK_nin[l]/n_bach
+                   
+    theta_model=theta_model-lr*gradK
+    sum_of_gradK=np.sum(gradK)
+    error_func=np.sum(np.abs(theta_model-J_vec))/d
+    print(t_gd,sum_of_gradK,error_func)
+
--- Minimum_Probability_Flow/Ising_Model/one-d_mpf/test-room/test.py
+++ Minimum_Probability_Flow/Ising_Model/one-d_mpf/test-room/test.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys
 vec=np.random.uniform(0.0,1.0,16)
 print(vec)
-fname="test-output"
+fname="test-output.txt"
 f=open(fname,"w")
 f.write(str(100))
 f.close()
