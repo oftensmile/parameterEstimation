6768409ff0eb9ca97682b5da2e19a205c1c1afc2
shimagaki <kai0shimagaki@gmail.com>
2016-10-06 20:14:56 +0900

    Added new programming

---
+++ Minimum_Probability_Flow/Ising_Model/one-d_mpf/check-lkhood.py
@@ -0,0 +1,30 @@
+import numpy as np
+from scipy import linalg 
+import matplotlib.pyplot as plt
+np.random.seed(0)
+n_sample=500
+mu,sig2=0.0,1.0
+nor=np.random.normal(mu,sig2,n_sample)
+nor2=np.random.normal(mu,sig2,n_sample*2)
+nor4=np.random.normal(mu,sig2,n_sample*4)
+nor8=np.random.normal(mu,sig2,n_sample*8)
+nor16=np.random.normal(mu,sig2,n_sample*16)
+"""
+plt.hist(nor,bins=30,alpha=0.5)
+plt.hist(nor2,bins=30,alpha=0.5)
+plt.hist(nor4,bins=30,alpha=0.5)
+plt.hist(nor8,bins=30,alpha=0.5)
+plt.hist(nor16,bins=30,alpha=0.5)
+"""
+#fig1=plt.figure()
+#ax1.plot()
+#ax1=fig1.add_subplot(111)
+plt.hist(np.log(nor),bins=30,alpha=0.5)
+plt.hist(np.log(nor2),bins=30,alpha=0.5)
+plt.hist(np.log(nor4),bins=30,alpha=0.5)
+#plt.hist(nor8,bins=30,alpha=0.5)
+#plt.hist(nor16,bins=30,alpha=0.5)
+#fig2=plt.figure()
+#ax2=fig2.add_subplot(111)
+plt.hist(nor,bins=30,alpha=0.5)
+plt.show()
