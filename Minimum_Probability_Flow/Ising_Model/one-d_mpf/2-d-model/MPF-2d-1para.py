1b9eea6c08797ff67cc6ccdd7b9032a6d60fbd1b
shimagaki <kai0shimagaki@gmail.com>
2016-10-26 15:18:20 +0900

    Added compleate 2-d, mpf,naiveCD,nomcCD,GD+mcmc

--- Minimum_Probability_Flow/Ising_Model/one-d_mpf/2-d-model/MPF-2d-1para.py
+++ Minimum_Probability_Flow/Ising_Model/one-d_mpf/2-d-model/MPF-2d-1para.py
@@ -9,8 +9,8 @@ t_interval = 100
 d, N_sample,N_model = 8,100,30 #124, 1000
 N_remove=100
 #parameter ( MPF+GD )
-lr,eps =0.0001, 1.0e-100
-t_gd_max=1000
+lr,eps =0.01, 1.0e-100
+t_gd_max=300
 
 class Matrices():
     def __init__(self,mat):
@@ -31,39 +31,9 @@ def gen_mcmc(J,x=[[]]):
                 x[i1][i2]=x[i1][i2]*(-1)
     return x
 
-def calc_E(J,x=[[]]):
-    erg=0
-    for i1 in range(d):
-        for i2 in range(d):
-            erg+=x[i1][i2]*(x[(i1+d-1)%d][i2]+x[(i1+1)%d][i2]+x[i1][(i2+d-1)%d]+x[i1][(i2+1)%d])
-    return 0.5*erg
-
-def get_E_C(J):
-    #x=np.ones((d,d))
-    x=np.random.choice([-1,1],(d,d))
-    E,E2=0.0,0.0
-    for n in range(N_sample+N_remove):
-        if(n<N_remove):
-            x=np.copy(gen_mcmc(J,x))
-        else:
-            for t in range(t_interval):
-                x=np.copy(gen_mcmc(J,x))
-            E_temp=calc_E(J,x)
-            E+=E_temp/N_sample
-            E2+=E_temp**2 / N_sample
-    return (E,-E2)
-
-def calc_C(x=[[]]):
-    c=0.0
-    for i1 in range(d):
-        for i2 in range(d):
-            c+=x[i1][i2]*(x[(i1+d-1)%d][i2]+x[(i1+1)%d][i2]+x[i1][(i2+d-1)%d]+x[i1][(i2+1)%d])
-    return 0.5*c
-
 if __name__ == '__main__':
     x=np.random.choice([-1,1],(d,d))
     J_data,J_model=0.5,0.1
-    C_data,C_model=0.0,0.0
     for n in range(N_sample+N_remove):
         if(n<N_remove):
             x=np.copy(gen_mcmc(J_data,x))
@@ -75,13 +45,17 @@ if __name__ == '__main__':
             for t in range(t_interval):
                 x=np.copy(gen_mcmc(J_data,x))
             data_matrix=np.append(data_matrix,Matrices(x))
-            C_data+=calc_C(x)/N_sample
     
     for t_gd in range(t_gd_max):
-        for m in range(N_model):
-            temp=np.random.choice(data_matrix)
-            xm=np.copy(gen_mcmc(J_model,temp.mat))
-            C_model+=calc_C(xm)/N_model
-        J_model=J_model + lr * (C_data - C_model)
-        C_model=0.0
+        gradK=0.0
+        for M in data_matrix:
+            x=np.copy(M.mat)
+            gradK1=0.0
+            for i1 in range(d):
+                for i2 in range(d):
+                    diff_E=-2*x[i1][i2]*(x[(i1+d-1)%d][i2]+x[(i1+1)%d][i2]+x[i1][(i2+d-1)%d]+x[i1][(i2+1)%d])
+                    gradK1+=diff_E*np.exp(0.5*J_model*diff_E)/(d**2)
+            gradK+=gradK1/N_sample
+        J_model-=lr*gradK
+        error=J_model-J_data
         print(abs(J_model-J_data))
