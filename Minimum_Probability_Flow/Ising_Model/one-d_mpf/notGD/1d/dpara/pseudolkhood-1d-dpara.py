0c528dd9e6bd07b73c9c1caf77a7b10cdb7778c1
shimagaki <kai0shimagaki@gmail.com>
2016-11-10 22:34:00 +0900

    Added PseudoLikelihood=>compleate 1D,d-th parameter

--- Minimum_Probability_Flow/Ising_Model/one-d_mpf/notGD/1d/dpara/pseudolkhood-1d-dpara.py
+++ Minimum_Probability_Flow/Ising_Model/one-d_mpf/notGD/1d/dpara/pseudolkhood-1d-dpara.py
@@ -3,26 +3,23 @@
 import numpy as np
 import time 
 from scipy import linalg
-import matplotlib.pyplot as plt
-import csv
-from scipy.optimize import fsolve
+from scipy.optimize import root 
 from scipy.optimize import newton 
-from scipy.optimize import minimize 
 import math
 n_estimation=300
 np.random.seed(0)
 t_interval = 10
 #parameter ( System )
-d, N_sample = 16,500 #124, 1000
-N_remove=30
+d, N_sample = 6,100 #124, 1000
+N_remove=100
 #parameter ( MPF+GD )
 lr,eps =0.1, 1.0e-100
 n_mfa = 50 #Number of the sample for Mean Field Aproximation.
 t_gd_max=100 
-def gen_mcmc(J,x=[] ):
+def gen_mcmc(J=[],x=[]):
     for i in range(d):
         #Heat Bath
-        diff_E=2.0*J*x[i]*(x[(i+d-1)%d]+x[(i+1)%d])#E_new-E_old
+        diff_E=2.0*x[i]*(J[(i+d-1)%d]*x[(i+d-1)%d]+J[i]*x[(i+1)%d])#E_new-E_old
         r=1.0/(1+np.exp(diff_E)) 
         R=np.random.uniform(0,1)
         if(R<=r):
@@ -70,13 +67,6 @@ def get_sample(j):
 def Obfunc_1d_1para(J,g_data_sum):
     return -g_data_sum+(d*np.cosh(J)*np.sinh(J)*(np.cosh(J)**(-2+d)+np.sinh(J)**(-2+d)))/(np.cosh(J)**d+np.sinh(J)**d)
 
-def myobj(J,i,X_sample):
-    a=0
-    for n in range(N_sample):
-        xn=np.copy(X_sample[n])
-        a+=2*xn[i]*xn[(i+1)%d]*(1+np.exp(-2*J*xn[i]*(xn[(i+d-1)%d]+xn[(i+1)%d])))/N_sample
-        a+=2*xn[i]*xn[(i+1)%d]*(1+np.exp(-2*J*xn[(i+1)%d]*(xn[i]+xn[(i+2)%d])))/N_sample
-    return a 
 def myobj(J,X_sample):
     J_est=np.zeros(d)
     for n in range(N_sample):
@@ -86,41 +76,44 @@ def myobj(J,X_sample):
             #Adding belof this condition is seems wrong
             J_est[i]+= xn[i]*xn[(i+1)%d]- xn[i]*np.tanh(xn[i]*J[i]+xn[(i+2)%d]*J[(i+1)%d])
     return J_est 
+
 if __name__ == '__main__':
-    sample_list=[500,1000,5000,10000,50000]
-    fname_sample="Pseudo-Likhood.dat"
-    F=open(fname_sample,"w")
-    for N_sample in sample_list:
+    #sample_list=[500,1000,5000,10000,50000]
+    #fname_sample="Pseudo-Likhood.dat"
+    #F=open(fname_sample,"w")
+    #for N_sample in sample_list:
         #fname="sample"+str(N_sample)+"-PLkhood.dat"
         #f=open(fname,"w")
-        J_model_list=np.zeros(n_estimation)
-        for nf in range(n_estimation):
-            J_data=1.0 # =theta_sample
-            #SAMPLING-Tmat
-            c_mean_data=0.0
-            for n in range(N_sample):
-                x=get_sample(J_data)
-                if(n==0):
-                    X_sample = np.copy(x)
-                elif(n>0):
-                    X_sample=np.vstack((X_sample,np.copy(x)))
-            
-            for n in range(N_sample):
-                x=get_sample(J_data)
-                if(n==0):X_sample = np.copy(x)
-                elif(n>0):X_sample=np.vstack((X_sample,np.copy(x)))
-            corre_sample_mean=calc_C(X_sample) 
-            J_model_vec=root(myobj,0.2*np.ones(d),args=(X_sample),method="hybr")
-            J_model=np.mean(J_model_vec.x)
-            J_model_list[nf]=J_model
-            """
-            for i in range(d):
-                Ji_newtoon=newton(myobj, 0.1,args=(i,X_sample,))
-                J_model_list[i]=Ji_newtoon
-            J_newton=np.mean(J_model_list)
-            f.write(str(J_newton)+"  "+str(np.abs(J_newton-J_data))+"\n")
-        f.write("#"+str(N_sample)+"  "+str(np.mean(J_model_list))+"  "+str(np.std(J_model_list))+"\n" )
-        f.close()
-       """
-        F.write(str(N_sample)+"  "+str(np.mean(J_model_list))+"  "+str(np.std(J_model_list))+"\n" )
-    F.close()
+    #J_model_list=np.zeros(n_estimation)
+        #for nf in range(n_estimation):
+    J_data=[0,1.0,1.0,0.5,0.0,1.0]
+    #SAMPLING-Tmat                      1
+    x=np.random.choice([-1,1],d)
+    for n in range(N_sample+N_remove):
+        for t in range(t_interval):
+            x = np.copy(gen_mcmc(J_data,x))
+        if(n==N_remove):
+            x_new=np.copy(x)
+            X_sample = np.copy(x)
+            #correlation_data+=calc_C(x_new)/N_sample
+        elif(n>N_remove):
+            x_new=np.copy(x)
+            X_sample=np.vstack((X_sample,np.copy(x)))
+            #correlation_data+=calc_C(x_new)/N_sample
+    
+    J_model=root(myobj,0.2*np.ones(d),args=(X_sample),method="hybr")
+    print("#J_data=",J_data)
+    print("#sol=",J_model.x)
+    print("#diff=",np.sum(np.abs(J_data-J_model.x)))
+
+    """
+    for i in range(d):
+        Ji_newtoon=newton(myobj, 0.1,args=(i,X_sample,))
+        J_model_list[i]=Ji_newtoon
+    J_newton=np.mean(J_model_list)
+    """
+            #f.write(str(J_newton)+"  "+str(np.abs(J_newton-J_data))+"\n")
+        #f.write("#"+str(N_sample)+"  "+str(np.mean(J_model_list))+"  "+str(np.std(J_model_list))+"\n" )
+        #f.close()
+        #F.write(str(N_sample)+"  "+str(np.mean(J_model_list))+"  "+str(np.std(J_model_list))+"\n" )
+    #F.close()
